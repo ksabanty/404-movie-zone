f283d4a022ffc5875b487541cb73208eec85b947
Kerry Sabanty <kgsabanty@gmail.com>
2024-03-12 18:54:58 -0400

    get single movie endpoint

--- movieAPI/src/main/java/dev/kgsabanty/movieAPI/MovieService.java
+++ movieAPI/src/main/java/dev/kgsabanty/movieAPI/MovieService.java
@@ -1,9 +1,11 @@
 package dev.kgsabanty.movieAPI;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -11,7 +13,11 @@ public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
 
-    public List<Movie> allMovies () {
+    public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
